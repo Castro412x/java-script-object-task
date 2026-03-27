803f6b51042eebfa1fadcd7e1c91aa44b0f549d5
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-27 12:04:26 +0100

    Add car description task

---
+++ question2.js
@@ -0,0 +1,12 @@
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2019,
+    price: 5500000
+};
+car.price=4800000
+car.color="white"
+console.log(car.brand)
+console.log(car.price)
+
+console.log(car)
